c9bba96f8566ab35585dbcdf500e520776930810
anxieuse <64018836+anxieuse@users.noreply.github.com>
2020-09-26 05:05:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,207 @@
+/*
+Короткевич Л. В.
+github.com/anxieuse
+Создать класс Address для работы с адресами домов. Адрес должен состоять из строк с названием города и улицы и чисел с номером дома и квартиры. 
+Реализовать операции сравнения адресов, а также операции проверки принадлежности адреса к улице и городу. 
+В операциях не должен учитываться регистр строки. 
+Так же необходимо сделать операцию, которая возвращает истину если два адреса находятся по соседству (на одной улице в одном городе и дома стоят подряд).
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
+
+// чтобы сравнивать строки без учета регистра
+std::string to_lower(const std::string &str)
+{
+    std::string copy = str;
+    for (auto &ch : copy)
+        ch = tolower(ch);
+    return copy;
+}
+
+class Address
+{
+public:
+    Address() : apt(0), house(0) {}
+    Address(const std::string &new_town, const std::string &new_street,
+            const int &new_apt, const int &new_house)
+    {
+        town = new_town;
+        street = new_street;
+        apt = new_apt;
+        house = new_house;
+    }
+    bool operator==(Address const &rhs) const
+    {
+        return to_lower(rhs.town) == to_lower(town) &&
+               to_lower(rhs.street) == to_lower(street) &&
+               rhs.apt == apt &&
+               rhs.house == house;
+    }
+    // принадлежность городу
+    bool inTown(std::string const &aTown) const
+    {
+        return std::equal(town.begin(), town.end(), aTown.begin(),
+                          [](char a, char b) {
+                              return tolower(a) == tolower(b);
+                          });
+    }
+    // принадлежность улице
+    bool inStreet(std::string const &aStreet) const
+    {
+        return std::equal(street.begin(), street.end(), aStreet.begin(),
+                          [](char a, char b) {
+                              return tolower(a) == tolower(b);
+                          });
+    }
+    // является ли соседом
+    bool isNeighs(Address const &rhs) const
+    {
+        if (!(to_lower(rhs.town) == to_lower(town) &&
+              to_lower(rhs.street) == to_lower(street)))
+            return false;
+        return abs(rhs.apt - apt) <= 1;
+    }
+    // вывести всю информацию об одном адресе
+    void display() const
+    {
+        std::cout << town << "\t" << street << "\t" << house << "\t" << apt;
+    }
+
+private:
+    std::string town, street; // название города, улицы
+    int apt, house;           // номер дома, квартиры
+};
+
+int main()
+{
+    std::string town, street;
+    int apt, house, idToDel, id, id1, id2;
+    std::vector<Address> ads;
+
+    int command_idx;
+    bool input = true;
+    std::cout << '\n'
+              << " 1  - добавить адрес" << '\n'
+              << " 2  - удалить адрес" << '\n'
+              << " 3  - сравнить два адреса" << '\n'
+              << " 4  - проверить принадлежность улице" << '\n'
+              << " 5  - проверить принадлежность городу" << '\n'
+              << " 6  - являются ли соседями" << '\n'
+              << " 7  - вывести список адресов" << '\n'
+              << " 8  - выйти\n"
+              << '\n';
+    while (input)
+    {
+        std::cin >> command_idx;
+        switch (command_idx)
+        {
+            case 1:
+            {
+                std::cin >> town >> street >> apt >> house;
+                printf("Адрес %s %s %d %d успешно добавлен в список\n", town.c_str(), street.c_str(), apt, house);
+                ads.push_back({town, street, apt, house});
+                break;
+            }
+            case 2:
+            {
+                std::cin >> idToDel;
+                --idToDel;
+                if (idToDel < ads.size() && idToDel >= 0)
+                {
+                    std::cout << "Адрес ";
+                    ads[idToDel].display();
+                    std::cout << " успешно удален из списка\n";
+                    ads.erase(ads.begin() + idToDel);
+                }
+                else
+                {
+                    std::cout << "Введен некорректный индекс\n";
+                }
+                break;
+            }
+            case 3:
+            {
+                std::cin >> id1 >> id2;
+                if (ads[id1 - 1] == ads[id2 - 1])
+                {
+                    std::cout << "Адреса равны\n";
+                }
+                else
+                {
+                    std::cout << "Адреса НЕ равны\n";
+                }
+                break;
+            }
+            case 4:
+            {
+                std::cin >> id >> street;
+                if (ads[id - 1].inStreet(street))
+                {
+                    printf("Адрес под номером %d действительно принадлежит улице %s\n", id, street.c_str());
+                }
+                else
+                {
+                    printf("Адрес под номером %d НЕ принадлежит улице %s\n", id, street.c_str());
+                }
+                break;
+            }
+            case 5:
+            {
+                std::cin >> id >> town;
+                if (ads[id - 1].inTown(town))
+                {
+                    printf("Адрес под номером %d действительно принадлежит городу %s\n", id, town.c_str());
+                }
+                else
+                {
+                    printf("Адрес под номером %d НЕ принадлежит городу %s\n", id, town.c_str());
+                }
+                break;
+            }
+            case 6:
+            {
+                std::cin >> id1 >> id2;
+                if (ads[id1 - 1].Address::isNeighs(ads[id2 - 1]))
+                {
+                    if (ads[id1 - 1] == ads[id2 - 1])
+                        printf("Адреса совпадают\n");
+                    else
+                        printf("Адреса под номерами %d и %d являются соседними\n", id1, id2);
+                }
+                else
+                {
+                    printf("Адреса под номерами %d и %d НЕ являются соседними\n", id1, id2);
+                }
+                break;
+            }
+            case 7:
+            {
+                if (ads.size() == 0)
+                {
+                    std::cout << "Список адресов пуст\n";
+                }
+                else
+                {
+                    printf("\n№\tГород\tУлица\tДом\tКватира\n");
+                    for (auto &x : ads)
+                    {
+                        std::cout << &x - &ads[0] + 1 << '\t';
+                        x.Address::display();
+                        std::cout << '\n';
+                    }
+                    std::cout << '\n';
+                }
+                break;
+            }
+            case 8:
+            {
+                input = false;
+                break;
+            }
+        }
+    }
+}
